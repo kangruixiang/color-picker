73857bda0da060923c94c5bea2eb5ac545257297
Kang Rui Xiang <kangruixiang@gmail.com>
2022-05-14 15:39:36 -0400

    Updates

---
+++ _app/pages/index.svelte-73473178.js
@@ -0,0 +1,5 @@
+import{S as v,i as x,s as g,e as h,t as d,c as m,a as f,h as u,d as l,b as _,g as b,J as r,n as o}from"../chunks/index-c3650d4a.js";function y(p){let t,n,e,i;return{c(){t=h("main"),n=d(`hello
+
+	`),e=h("div"),i=d("this is a test"),this.h()},l(s){t=m(s,"MAIN",{class:!0});var a=f(t);n=u(a,`hello
+
+	`),e=m(a,"DIV",{class:!0});var c=f(e);i=u(c,"this is a test"),c.forEach(l),a.forEach(l),this.h()},h(){_(e,"class","font-serif"),_(t,"class","container h-screen bg-slate-200")},m(s,a){b(s,t,a),r(t,n),r(t,e),r(e,i)},p:o,i:o,o,d(s){s&&l(t)}}}class I extends v{constructor(t){super(),x(this,t,null,y,g,{})}}export{I as default};
--- src/routes/index.svelte
+++
@@ -1,5 +0,0 @@
-<main class="container h-screen bg-slate-200">
-	hello
-
-	<div class="font-serif">this is a test</div>
-</main>
